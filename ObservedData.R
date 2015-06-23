0cfcf9b37d65d00fe82d49709f504b1a57c5dbfe
kbrannan <brannan.kevin@deq.state.or.us>
2015-06-23 12:24:47 -0700

    R sccript to load and precess observed flow data

---
+++ ObservedData.R
@@ -0,0 +1,18 @@
+## script reads in and adds date (in POSIXct format) observed flow data
+## for OWRD station 14306030. I am using publised and provisional mean 
+## data daily flow data (in cfs) for the period of 1995-10-01 to 
+## 2014-09-30. I downloaded the data manually from:
+## http://apps.wrd.state.or.us/apps/sw/hydro_near_real_time/display_hydro_graph.aspx?station_nbr=14306030
+
+
+options(stringsAsFactors=FALSE)
+
+chr.dir <- paste0(dirname(getwd()),"/data")
+chr.file <- "Station_14306030_mean_daily_flow.txt"
+
+df.obs <- read.table(file=paste0(chr.dir,"/",chr.file), header=TRUE, sep="\t")
+
+df.obs <-cbind(df.obs,date=strptime(df.obs$record_date,format="%m-%d-%Y"))
+
+rm(list=ls()[-grep("df.obs",ls())])
+
