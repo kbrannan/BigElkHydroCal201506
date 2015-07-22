46a868f6b31c1e70ffb9047d6f65dd5d7c57119f
kbrannan <brannan.kevin@deq.state.or.us>
2015-07-22 16:07:23 -0700

    added some stuff before shiny

--- StormSelection.R
+++ StormSelection.R
@@ -1,11 +1,45 @@
-install.packages(c("DVstats"), 
-                 repos=c("http://usgs-r.github.com","http://cran.us.r-project.org"), 
-                 dependencies=TRUE)
+## USGS DVstats package
+## to install package from GitHub use this command:
+## install.packages(c("DVstats"),repos=c("http://usgs-r.github.com","http://cran.us.r-project.org"), dependencies=TRUE)
 library(DVstats)
+library(ggplot2)
 
-df.hydsep.est <- hysep(Flow=df.est[,3],Dates=as.Date(df.est[,8]),da=88.8,select="sliding")
+## seperate hydrograph
+## using "hysep" because the "part" function is not documented enough to understand the output
+bf.est <- hysep(Flow=df.est$mean_daily_flow_cfs,Dates=as.Date(df.est$record_date,format="%m-%d-%Y"),da=88.8,select="sliding",STAID="BigElk")
 
-ls.recess.est <- recess(Flow=df.est[,3],Dates=as.Date(df.est[,8]))
+## plot baseflow and total flow
+plot.bf.est <- ggplot() + xlab("") +
+  geom_line(data=bf.est,stat="identity",aes(x=Dates,y=Flow,colour="blue")) +
+  geom_line(data=bf.est,stat="identity",aes(x=Dates,y=BaseQ,colour="red"))+
+  scale_y_log10("Mean Daily Flow (cfs)")
+plot(plot.bf.est)
 
-ls.recess.est$Recessions
+## use R command "peaks to get inflection points
+library(smwrBase)
+spn <- 5
+yr.b <- 1999
 
+tmp.peaks.R <- df.est[peaks(df.est$mean_daily_flow_cfs,span=spn) == TRUE,]
+tmp.peaks.R <- cbind(tmp.peaks.R,Dates=as.Date(tmp.peaks.R$record_date,format="%m-%d-%Y"))
+tmp.rises.R <- df.est[peaks(-1*df.est$mean_daily_flow_cfs,span=spn) == TRUE,]
+tmp.rises.R <- cbind(tmp.rises.R,Dates=as.Date(tmp.rises.R$record_date,format="%m-%d-%Y"))
+## plot baseflow and total flow for a water year
+
+dt.b <- as.Date(paste0(yr.b,"/10/01"))
+dt.e <- as.Date(paste0(as.numeric(format(dt.b,"%Y")) + 1,"/09/30"))
+if(dt.e > max(bf.est$Dates)) dt.e <- max(bf.est$Dates)
+df.yr <- bf.est[bf.est$Dates >= dt.b & bf.est$Dates <= dt.e, ]
+df.peak <- tmp.peaks.R[tmp.peaks.R$Dates >= dt.b & tmp.peaks.R$Dates <= dt.e, ]
+df.rise <- tmp.rises.R[tmp.rises.R$Dates >= dt.b & tmp.rises.R$Dates <= dt.e, ]
+plot.peak.rise.yr <- ggplot(data=df.yr) + xlab("") +
+  geom_line(stat="identity",aes(x=Dates,y=Flow,colour="blue")) +
+  geom_line(stat="identity",aes(x=Dates,y=BaseQ,colour="red"))+
+  geom_point(data=df.peak,aes(x=Dates,y=mean_daily_flow_cfs)) +
+  geom_point(data=df.rise,aes(x=Dates,y=mean_daily_flow_cfs)) +
+  scale_y_log10("Mean Daily Flow (cfs)") + theme(legend.position="none")
+plot(plot.peak.rise.yr)
+
+
+## shiny
+library(shiny)
