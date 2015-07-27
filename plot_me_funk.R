cf418335c76ad94a88ed442c1879bf8fbaad197c
kbrannan <brannan.kevin@deq.state.or.us>
2015-07-27 16:54:32 -0700

    automated the limits for the plot_me function

--- plot_me_funk.R
+++ plot_me_funk.R
@@ -16,13 +16,14 @@ plot.me <- function(spn,yr.b,dates,flow) {
   dt.e <- as.Date(paste0(as.numeric(format(dt.b,"%Y")) + 1,"/09/30"))
   
   df.yr <- df.tmp[df.tmp$date >= dt.b & df.tmp$date <= dt.e, ]
+  df.yr.ylims <- c(10^(floor(log10(min(df.yr$flow))-1)),10^(ceiling(log10(max(df.yr$flow))+1)))
   df.peak <- tmp.peaks[tmp.peaks$date >= dt.b & tmp.peaks$date <= dt.e, ]
   df.rise <- tmp.rises[tmp.rises$date >= dt.b & tmp.rises$date <= dt.e, ]
   plot.peak.rise.yr <- ggplot() + xlab("") + ggtitle(paste0("Span = ",spn," days")) +
     geom_line(data=df.yr,stat="identity",aes(x=dates,y=flow, colour="blue")) +
     geom_point(data=df.peak,aes(x=dates,y=flow,colour="red",size=1)) +
     geom_point(data=df.rise,aes(x=dates,y=flow,colour="green",size=1)) +
-    scale_y_log10("Mean Daily Flow (cfs)") + theme(legend.position="none")
+    scale_y_log10("Mean Daily Flow (cfs)",limits=df.yr.ylims) + theme(legend.position="none")
 
   return(plot.peak.rise.yr)
 }
