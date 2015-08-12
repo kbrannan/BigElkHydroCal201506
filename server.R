ab98c553cd575f526d4e2f3a4fdea080cee10ba2
kbrannan <brannan.kevin@deq.state.or.us>
2015-08-12 16:22:16 -0700

    can get date by clicking the storm on image now using the base graphics plot

--- server.R
+++ server.R
@@ -1,7 +1,9 @@
 # server.R
 
-library(shiny)
-library(Rcpp)
+require(shiny)
+require(Rcpp)
+require(DT)
+
 
 shinyServer(function(input,output) {
   
@@ -14,30 +16,16 @@ shinyServer(function(input,output) {
     )
     output$plot <- renderPlot( 
       {
-#       plot(plot.me(spn=input$spn,
-#                    yr.b=input$yr.b,
-#                    dates=df.est$date,
-#                    flow=df.est$mean_daily_flow_cfs,
-#                    lst.pot.strm=getPotentialStormData(spn=input$spn,
-#                                                       dates=df.est$date,
-#                                                       flow=df.est$mean_daily_flow_cfs)
-#                    )
-#            )
         spn <- input$spn
-        
         lst.pot.strm <-getPotentialStormData(spn=input$spn,dates=df.est$date,flow=df.est$mean_daily_flow_cfs)
-        
         yr.b <- input$yr.b
         df.tmp <- data.frame(dates=as.Date(df.est$date),flow=df.est$mean_daily_flow_cfs)
-        
         tmp.peaks <- lst.pot.strm$peaks
         tmp.rises <- lst.pot.strm$rises
         tmp.rises.sel <- lst.pot.strm$rises.sel
         tmp.pot.strms <- lst.pot.strm$pot.strm
-        
         dt.b <- as.Date(paste0(yr.b,"/10/01"))
         dt.e <- as.Date(paste0(as.numeric(format(dt.b,"%Y")) + 1,"/09/30"))
-        
         df.yr <- df.tmp[df.tmp$date >= dt.b & df.tmp$date <= dt.e, ]
         df.yr.ylims <- c(10^(floor(log10(min(df.yr$flow))-1)),10^(ceiling(log10(max(df.yr$flow))+1)))
         df.yr.xlims <- c(dt.b,dt.e)
@@ -57,23 +45,6 @@ shinyServer(function(input,output) {
     
     format(as.Date(input$plot_click$x),format="%Y-%m-%d")
     })
-#   output$quick <- renderPrint({nearPoints(df.est, input$plot_click, xvar = "date", yvar = "mean_daily_flow_cfs")
-#   })
-#  )
-
-#output$quick <- renderText(input$plot_click$x)
-#output$quick <- renderText(if(is.numeric(input$plot_click$x) == TRUE) findStorm(dt.strm=getPlotDate(input$yr.b,input$plot_click$x),
-#                                     z=datasetInput()))
-# output$quick <- renderPrint(if(is.numeric(input$plot_click$x) == TRUE) getPlotDate(dt.bnd=format(plot.me(spn=input$spn,
-#                                   yr.b=input$yr.b,
-#                                   dates=df.est$date,
-#                                   flow=df.est$mean_daily_flow_cfs,
-#                                   lst.pot.strm=datasetInput()
-#                                   )$coordinates$limits, format="%Y-%m-%d"),
-#                                   frac=input$plot_click$x)  
-#                            )
-
-
 
     output$table <- renderDataTable(table.me(yr.b=input$yr.b,
                                             z=datasetInput()))
