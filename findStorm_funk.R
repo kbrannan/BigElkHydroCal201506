04779276a082f00940580879a7bfb8ecd4ec0821
kbrannan <brannan.kevin@deq.state.or.us>
2015-08-12 17:03:52 -0700

    use length instead of is.Date to check for storm in findStorm

--- findStorm_funk.R
+++ findStorm_funk.R
@@ -1,7 +1,7 @@
 findStorm <- function(dt.strm,x) {
   if(is.Date(as.Date(dt.strm)) == TRUE) {
       row.strm <- x[x$date.bgn <= dt.strm & x$date.end >= dt.strm, ]
-      if(is.Date(row.strm[1,1])) {
+      if(length(row.strm[,1] > 0)) {
         str.info <- paste0(format(row.strm[1,1], format="%Y-%m-%d"), " to ", 
                            format(row.strm[1,2], format="%Y-%m-%d"), " with duration of ",
                            format(row.strm[1,3], format="%02i"), " days, peak flow of ",
